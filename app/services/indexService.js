7ab5f4858448dd43437f64a50478def4ba5d9ff6
Takeshi Mochida <takeshimochida@gmail.com>
2015-04-24 13:48:23 -0700

    Remove unused service

--- app/services/indexService.js
+++
@@ -1,9 +0,0 @@
-// Service to keep track of navbar collapse state in mobile view
-
-app.factory('indexService', function() {
-    var pageIndex = -1;
-    return {
-        index: function() { return pageIndex; },
-        setIndex: function(index) { pageIndex = index; }
-    };
-});
