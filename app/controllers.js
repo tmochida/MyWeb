78e26a290bf39d886fc7e05eea87efddb42ef7f7
Takeshi Mochida <takeshimochida@gmail.com>
2015-07-21 22:30:18 -0700

    Better styling, update footer and pages

--- app/controllers.js
+++ app/controllers.js
@@ -15,6 +15,11 @@ app.controller('mainCtrl', function($scope, infoService, stateService, $location
     
     infoService.getTitles(); // get titles for all pages
     infoService.getTranslations($scope, 'main');
+
+    // set offset
+    $scope.offsetHeight = (stateService.height()-140)/3;
+
+    $scope.email = "tmochida@takeshimochida.com";
 });
 
 // Controllers for each page
@@ -25,32 +30,32 @@ app.controller('homeCtrl', function($scope, $location, infoService) {
     } else {
         infoService.setClass('home');
     }
-    infoService.setDescription("Takeshi Mochida. Student and Developer.");
+    infoService.setDescription("Takeshi Mochida studies Computer Science at the University of California, Berkeley.");
     infoService.getTranslations($scope, $scope.pageClass);
 });
 
 app.controller('aboutCtrl', function($scope, infoService) {
     $scope.pageClass = 'about';
     infoService.setClass('about');
-    infoService.setDescription("Takeshi Mochida. About Me");
+    infoService.setDescription("About Takeshi Mochida.");
 });
 
 app.controller('resumeCtrl', function($scope, infoService) {
     $scope.pageClass = 'resume';
     infoService.setClass('resume');
-    infoService.setDescription("Takeshi Mochida. Resume");
+    infoService.setDescription("Takeshi Mochida's resume");
 });
 
 app.controller('projectsCtrl', function($scope, infoService) {
     $scope.pageClass = 'projects';
     infoService.setClass('projects');
-    infoService.setDescription("Takeshi Mochida. Projects");
+    infoService.setDescription("Projects worked on by Takeshi Mochida.");
 });
 
 app.controller('contactCtrl', function($scope, infoService) {
     $scope.pageClass = 'contact';
     infoService.setClass('contact');
-    infoService.setDescription("Takeshi Mochida. Lets get in touch.");
+    infoService.setDescription("How to get in touch with Takeshi Mochida.");
     infoService.getTranslations($scope, $scope.pageClass);
 });
 
